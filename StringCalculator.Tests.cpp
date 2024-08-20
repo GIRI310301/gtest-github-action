5183f65d4521f41a7be8c62ab195dbbbb904f857
GIRI310301 <rgirish2001@gmail.com>
2024-08-20 15:19:16 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -12,3 +12,34 @@ TEST(string_calculator_add_when_passed_a_single_number, returns_0_for_empty_stri
   //Assert
   ASSERT_EQ(actualSum,expectedSum);
 }
+
+TEST(string_calculator_add_when_passed_a_single_number, returns_0_for_zero) 
+{
+  StringCalculator calculator;
+  string inp = "0";
+  int expectedSum = 0;
+  int actualSum = calculator.Add(inp);
+
+  ASSERT_EQ(actualSum,expectedSum);
+}
+
+TEST(string_calculator_add_when_passed_a_single_number, returns_1_for_one) 
+{
+  StringCalculator calculator;
+  string inp = "1";
+  int expectedSum = 1;
+  int actualSum = calculator.Add(inp);
+
+  ASSERT_EQ(actualSum,expectedSum);
+}
+
+
+TEST(string_calculator_add_when_passed_two_comma_delimited_numbers, returns_their_sum) 
+{
+  StringCalculator calculator;
+  string inp = "1,2";
+  int expectedSum = 3;
+  int actualSum = calculator.Add(inp);
+
+  ASSERT_EQ(actualSum,expectedSum);
+}
