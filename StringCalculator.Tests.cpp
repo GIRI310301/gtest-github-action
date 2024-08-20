727b09acca6342e8140f5564ac66a1cfa5b7deb7
GIRI310301 <rgirish2001@gmail.com>
2024-08-20 15:35:25 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -33,7 +33,6 @@ TEST(string_calculator_add_when_passed_a_single_number, returns_1_for_one)
   ASSERT_EQ(actualSum,expectedSum);
 }
 
-
 TEST(string_calculator_add_when_passed_two_comma_delimited_numbers, returns_their_sum) 
 {
   StringCalculator calculator;
@@ -42,4 +41,36 @@ TEST(string_calculator_add_when_passed_two_comma_delimited_numbers, returns_thei
   int actualSum = calculator.Add(inp);
 
   ASSERT_EQ(actualSum,expectedSum);
+}
+
+TEST(string_calculator_add_when_passed_multiple_comma_delimited_numbers, returns_their_sum) 
+{
+  StringCalculator calculator;
+  string inp = "1,2,3";
+  int expectedSum = 6;
+  int actualSum = calculator.Add(inp);
+
+  ASSERT_EQ(actualSum,expectedSum);
+}
+
+TEST(string_calculator_add_when_delimited_with_newline_and_comma, returns_their_sum) 
+{
+  StringCalculator calculator;
+  string inp = "1\n2,3";
+  int expectedSum = 3;
+  int actualSum = calculator.Add(inp);
+
+  ASSERT_EQ(actualSum,expectedSum);
+}
+
+TEST(string_calculator_add_when_passed_negative_numbers, throws_an_exception_listing_invalid_values) 
+{
+  StringCalculator calculator;
+  string inp = "1,-2,-4,5";
+  //int expectedSum = 3;
+  int actualSum = calculator.Add(inp);
+
+  EXPECT_THROW(actualSum, invalid_argument);
+  //ASSERT_EQ(actualSum,expectedSum);
+  ASSERT_EQ(LastException<std::invalid_argument>().what(), "Negatives not allowed: -2,-4");
 }
