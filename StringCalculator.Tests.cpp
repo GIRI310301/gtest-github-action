d96771d35ff5749834bf1f4362d4a4736385932f
GIRI310301 <rgirish2001@gmail.com>
2024-08-20 16:46:12 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,10 +1,12 @@
 #include <gtest/gtest.h>
 #include "StringCalculator.h"
 
+StringCalculator calculator;
+
 TEST(string_calculator_add_when_passed_a_single_number, returns_0_for_empty_string) 
 {
   //Arrange
-  StringCalculator calculator;
+  //StringCalculator calculator;
   string inp = "";
   int expectedSum = 0;
   //Act
@@ -15,7 +17,7 @@ TEST(string_calculator_add_when_passed_a_single_number, returns_0_for_empty_stri
 
 TEST(string_calculator_add_when_passed_a_single_number, returns_0_for_zero) 
 {
-  StringCalculator calculator;
+  //StringCalculator calculator;
   string inp = "0";
   int expectedSum = 0;
   int actualSum = calculator.Add(inp);
@@ -25,7 +27,7 @@ TEST(string_calculator_add_when_passed_a_single_number, returns_0_for_zero)
 
 TEST(string_calculator_add_when_passed_a_single_number, returns_1_for_one) 
 {
-  StringCalculator calculator;
+  //StringCalculator calculator;
   string inp = "1";
   int expectedSum = 1;
   int actualSum = calculator.Add(inp);
@@ -35,7 +37,7 @@ TEST(string_calculator_add_when_passed_a_single_number, returns_1_for_one)
 
 TEST(string_calculator_add_when_passed_two_comma_delimited_numbers, returns_their_sum) 
 {
-  StringCalculator calculator;
+  //StringCalculator calculator;
   string inp = "1,2";
   int expectedSum = 3;
   int actualSum = calculator.Add(inp);
@@ -45,7 +47,7 @@ TEST(string_calculator_add_when_passed_two_comma_delimited_numbers, returns_thei
 
 TEST(string_calculator_add_when_passed_multiple_comma_delimited_numbers, returns_their_sum) 
 {
-  StringCalculator calculator;
+  //StringCalculator calculator;
   string inp = "1,2,3";
   int expectedSum = 6;
   int actualSum = calculator.Add(inp);
@@ -55,7 +57,7 @@ TEST(string_calculator_add_when_passed_multiple_comma_delimited_numbers, returns
 
 TEST(string_calculator_add_when_delimited_with_newline_and_comma, returns_their_sum) 
 {
-  StringCalculator calculator;
+  //StringCalculator calculator;
   string inp = "1\n2,3";
   int expectedSum = 6;
   int actualSum = calculator.Add(inp);
@@ -65,7 +67,7 @@ TEST(string_calculator_add_when_delimited_with_newline_and_comma, returns_their_
 
 TEST(string_calculator_add_when_passed_negative_numbers, throws_an_exception_listing_invalid_values) 
 {
-  StringCalculator calculator;
+  //StringCalculator calculator;
   string inp = "1,-2,-4,5";
 
   ASSERT_THROW(calculator.Add(inp),invalid_argument);
@@ -73,7 +75,7 @@ TEST(string_calculator_add_when_passed_negative_numbers, throws_an_exception_lis
 
 TEST(string_calculator_add_when_passed_numbers_over_1000, ignores_them) 
 {
-  StringCalculator calculator;
+  //StringCalculator calculator;
   string inp = "42,1001,3";
   int expectedSum = 45;
   int actualSum = calculator.Add(inp);
@@ -83,7 +85,7 @@ TEST(string_calculator_add_when_passed_numbers_over_1000, ignores_them)
 
 TEST(string_calculator_add_when_passed_multicharacter_delimiter, uses_that_delimiter_to_sum_values) 
 {
-  StringCalculator calculator;
+  //StringCalculator calculator;
   string inp = "//[***]\n8***2***3";
   int expectedSum = 13;
   int actualSum = calculator.Add(inp);
@@ -93,7 +95,7 @@ TEST(string_calculator_add_when_passed_multicharacter_delimiter, uses_that_delim
 
 TEST(string_calculator_add_when_passed_multiple_delimiters, sums_on_each_delimiter) 
 {
-  StringCalculator calculator;
+  //StringCalculator calculator;
   string inp = "//[*][%]\n4*2%3";
   int expectedSum = 9;
   int actualSum = calculator.Add(inp);
@@ -103,7 +105,7 @@ TEST(string_calculator_add_when_passed_multiple_delimiters, sums_on_each_delimit
 
 TEST(string_calculator_add_when_passed_multiple_multicharacter_delimiters, sums_on_each_delimiter) 
 {
-  StringCalculator calculator;
+  //StringCalculator calculator;
   string inp = "//[**][%^]\n4**1%^9";
   int expectedSum = 14;
   int actualSum = calculator.Add(inp);
