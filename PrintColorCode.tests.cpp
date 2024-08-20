e780026c71726fd5ab8f2139697a806663e5f805
GIRI310301 <rgirish2001@gmail.com>
2024-08-20 14:36:03 +0530

    Update PrintColorCode.tests.cpp

--- PrintColorCode.tests.cpp
+++ PrintColorCode.tests.cpp
@@ -1,1 +1,10 @@
 #include "PrintColorCode.h"
+#include <gtest/getest.h>
+
+TEST(PrintColorCodeTestSuite, ColorCodeManualCount)
+{
+  int count = 25;
+  int actualCount = printColorMap();
+  ASSERT_EQ(count,actualCount);
+}
+
