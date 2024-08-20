6f961ec1e76d45fbd2ac51f7011b96013e5c7c5b
GIRI310301 <rgirish2001@gmail.com>
2024-08-20 14:41:16 +0530

    Update PrintColorCode.cpp

--- PrintColorCode.cpp
+++ PrintColorCode.cpp
@@ -13,10 +13,3 @@ int printColorMap() {
     }
     return i * j;
 }
- 
-int main() {
-    int result = printColorMap();
-    assert(result == 25);
-    std::cout << "All is well (maybe!)\n";
-    return 0;
-}
