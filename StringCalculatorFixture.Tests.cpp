d218d2f32e435931e5b986afb6ec7e3e0eae8b4c
GIRI310301 <rgirish2001@gmail.com>
2024-08-21 10:52:16 +0530

    Update StringCalculatorFixture.Tests.cpp

--- StringCalculatorFixture.Tests.cpp
+++ StringCalculatorFixture.Tests.cpp
@@ -5,8 +5,28 @@
 class StringCalculatorAddTestFixture:public testing::Test{
   protected:
     //Common Reusable Code
-    StringCalculator calculator;
+    StringCalculator *calculator;
     //Hook Callbacks
+  // You can define per-test-suite set-up logic as usual.
+     static void SetUpTestSuite() {
+       std::cout<<"StringCalculatorAddTestFixture Setup"<<std::endl;
+     }
+ // You can define per-test-suite tear-down logic as usual.
+     static void TearDownTestSuite() {
+       std::cout<<"StringCalculatorAddTestFixture TearDown"<<std::endl;
+     }
+  // You can define per-test set-up logic as usual.
+  void SetUp() override {
+    calculator=new StringCalculator();
+    std::cout<<"Test Setup"<<std::endl;
+  }
+
+  // You can define per-test tear-down logic as usual.
+  void TearDown() override {
+    delete calculator;
+    std::cout<<"Test Teardown"<<std::endl;
+  }
+
 };
 
 TEST_F(StringCalculatorAddTestFixture,when_passed_a_single_number_returns_0_for_empty_string)
