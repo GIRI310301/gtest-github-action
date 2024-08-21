5ae0ed8a50613311d6d337060fd63bd6fa0a21c4
GIRI310301 <rgirish2001@gmail.com>
2024-08-21 16:15:44 +0530

    Update Celsius.h

--- Celsius.h
+++ Celsius.h
@@ -1,6 +1,5 @@
-
 // int networkAlerter(float celcius);
 // void alertInCelcius(float farenheit);
 
-int alertFailureCount = 0;
+extern int alertFailureCount = 0;
 void alertInCelcius(float farenheit,int(*networkAleter)(float celcius));
