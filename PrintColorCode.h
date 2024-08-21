01f36a57d95cf271f675d54739a2582d9a6703cf
GIRI310301 <rgirish2001@gmail.com>
2024-08-21 13:07:14 +0530

    Update PrintColorCode.h

--- PrintColorCode.h
+++ PrintColorCode.h
@@ -1,2 +1,6 @@
 
-int printColorMap();
+//While having error
+//int printColorMap();
+
+//After refactoring
+int printColorMap(void(*printer)(int colorCode,const char* majorColor,const char* mincorColor));
