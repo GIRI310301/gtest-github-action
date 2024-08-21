d00b1b4b83d16ed05323b6b2414ea9fea4489134
GIRI310301 <rgirish2001@gmail.com>
2024-08-21 15:33:46 +0530

    Update Celsius.cpp

--- Celsius.cpp
+++ Celsius.cpp
@@ -9,8 +9,7 @@ int networkAlerter(float celcius) {
    //Real Network Interaction Happens Here
     // Return 200 for ok
     // Return 500 for not-ok
-  //if(celcius % 2==0){
-  if (std::fmod(celcius, 2.0) == 0.0) { 
+  if(celcius % 2==0){
     return 200;
   }
   return 500;
