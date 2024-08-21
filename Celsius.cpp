c5e71dc22372c7156683495fa0505b6516b18e7c
GIRI310301 <rgirish2001@gmail.com>
2024-08-21 16:12:23 +0530

    Update Celsius.cpp

--- Celsius.cpp
+++ Celsius.cpp
@@ -3,23 +3,23 @@
 #include "Celsius.h"
 
 
-int alertFailureCount = 0;
+// int alertFailureCount = 0;
 
-int networkAlerter(int celcius) {
-   //Real Network Interaction Happens Here
-    // Return 200 for ok
-    // Return 500 for not-ok
-   //if (std::fmod(celcius, 2.0) == 0.0) {
-  if(celcius % 2==0){
-    return 200;
-  }
-  return 500;
-}
+// int networkAlerter(int celcius) {
+//    //Real Network Interaction Happens Here
+//     // Return 200 for ok
+//     // Return 500 for not-ok
+//    //if (std::fmod(celcius, 2.0) == 0.0) {
+//   if(celcius % 2==0){
+//     return 200;
+//   }
+//   return 500;
+// }
 
-void alertInCelcius(float farenheit) {
-    int celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = networkAlerter(celcius);
-    if (returnCode != 200) {
-      alertFailureCount += 0;
-    }
-}
+// void alertInCelcius(float farenheit) {
+//     int celcius = (farenheit - 32) * 5 / 9;
+//     int returnCode = networkAlerter(celcius);
+//     if (returnCode != 200) {
+//       alertFailureCount += 0;
+//     }
+// }
