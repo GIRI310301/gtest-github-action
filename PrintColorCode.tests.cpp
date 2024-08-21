00812bee51609d551982e235c996016a12a0f329
GIRI310301 <rgirish2001@gmail.com>
2024-08-21 13:09:33 +0530

    Update PrintColorCode.tests.cpp

--- PrintColorCode.tests.cpp
+++ PrintColorCode.tests.cpp
@@ -1,10 +1,24 @@
 #include "PrintColorCode.h"
 #include <gtest/gtest.h>
 
-TEST(PrintColorCodeTestSuite, ColorCodeManualCount)
-{
-  int count = 25;
-  int actualCount = printColorMap();
-  ASSERT_EQ(count,actualCount);
-}
+// Having an error
+// TEST(PrintColorCodeTestSuite, ColorCodeManualCount)
+// {
+//   int count = 25;
+//   int actualCount = printColorMap();
+//   ASSERT_EQ(count,actualCount);
+// }
 
+//After refactoring, Here we use STUB as a Interation Testing (Fake function in place of printOnTerminal function)
+//This is also known as TestDouble concept
+void FakePrinter(int colorCode, const char* majorColor,const char* minorColor){
+  
+}
+TEST(PrintColorCodeTestSuite, AssertColorCodeManualItemsCount){
+//Arrange
+  int expectedCount=25;
+ //Act
+  int actualCount=printColorMap(&FakePrinter);
+  //Assert
+  ASSERT_EQ(expectedCount,actualCount);
+}
