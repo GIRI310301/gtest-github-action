2a6f179e0a75ff095144c1a4b41ed3a920350c12
GIRI310301 <rgirish2001@gmail.com>
2024-08-21 16:13:11 +0530

    Update Celsius.cpp

--- Celsius.cpp
+++ Celsius.cpp
@@ -23,3 +23,26 @@
 //       alertFailureCount += 0;
 //     }
 // }
+
+#include <iostream>
+#include "TestChallenge.h"
+
+
+
+int realTimeNetworkAlerter(float celcius) {
+   //Real Network Interaction Happens Here
+    // Return 200 for ok
+    // Return 500 for not-ok
+  //if(celcius % 2==0){
+    //return 200;
+  //}
+  return 500;
+}
+
+ void alertInCelcius(float farenheit,int(*networkAlerter)(float celcius)) {
+    float celcius = (farenheit - 32) * 5 / 9;
+    int returnCode = networkAlerter(celcius);
+    if (returnCode != 200) {
+      alertFailureCount += 1;
+    }
+}
