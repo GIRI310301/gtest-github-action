6aa9f5f452bbfb60374703dec9f65b02ac7f0dbc
GIRI310301 <rgirish2001@gmail.com>
2024-08-21 16:13:50 +0530

    Update Celsius.Tests.cpp

--- Celsius.Tests.cpp
+++ Celsius.Tests.cpp
@@ -39,3 +39,43 @@
 
 //     ASSERT_EQ(alertFailureCount, 1);
 // }
+
+int networkAltererStub(float celcius){
+    return 500;
+}
+
+float celciusArg;
+int callCount;
+int networkAltererMock(float celcius){
+      celciusArg=celcius;
+      ++callCount;
+  return 500;
+}
+
+TEST(TestChallengeTestSuite, StateTesting){
+//Arrange
+  int expectedFailureCount=1;
+  float inputTemp = 100;
+   // alertFailureCount=0;
+ //Act
+  alertInCelcius(inputTemp,&networkAltererStub);
+  //Assert
+  ASSERT_EQ(expectedFailureCount, alertFailureCount);
+  
+}
+TEST(TestChallengeTestSuite, InteractionTesting){
+//Arrange
+  int expectedFailureCount=1;
+  float inputTempInF = 100;
+  alertFailureCount=0;
+  float expectedCelciusToBeReceivedByDependency=37.7778;
+ //Act
+  alertInCelcius(inputTempInF,&networkAltererMock);
+  
+  //Assert
+  ASSERT_EQ(expectedFailureCount, alertFailureCount);//state Based 
+  ASSERT_EQ(celciusArg, expectedCelciusToBeReceivedByDependency);//state Based 
+  ASSERT_EQ(callCount,1);
+  
+  
+}
